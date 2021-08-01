f60e403e23d7a535aa28ac64a4b59564d6dc42d2
guddu lawaniya <guddulawaniya123@gmail.com>
2021-08-01 11:08:01 +0530

    share meme app

--- app/src/main/java/com/example/sharememe/MainActivity.kt
+++ app/src/main/java/com/example/sharememe/MainActivity.kt
@@ -2,22 +2,19 @@ package com.example.sharememe
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.cronet.CronetHttpStack
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import org.w3c.dom.Text
 
 var currentimageurl: String? = null
 
@@ -79,9 +76,8 @@ class MainActivity : AppCompatActivity() {
     fun sharebutton(view: View) {
 
         val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "text/plain"
+        intent.type = "image/jpg"
         intent.putExtra(Intent.EXTRA_TEXT,"Hey Checkout this cool meme i got From Reddit")
-        val chooser = Intent.createChooser(intent,"Share this meme using ")
-        startActivity(intent)
+        startActivity(Intent.createChooser(intent, "Share Image this meme"))
     }
 }
